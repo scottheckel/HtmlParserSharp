14ed8ffdc5507866a68426ac0eb7cd46b21e461f
jamietre <jamietre@gmail.com>
2012-09-26 17:56:50 -0400

    WIP towards creating a consistent public API

--- HtmlParserSharp.Tests/Basic.cs
+++ HtmlParserSharp.Tests/Basic.cs
@@ -6,25 +6,14 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace HtmlParserSharp.Tests
 {
     [TestClass]
-    public class BigDom
+    public class Basic
     {
         static CQ Dom;
 
         /// <summary>
-        /// This method ensures that the huge DOM gets parsed correctly by checking a few key selectors. The biggest factors that
-        /// can cause problems are self-closing tags. See: TagHasImplicitClose function.
-        /// 
-        ///  Note that changing in that function
-        ///  
-        ///  case DomData.tagLI:
-        ///     return newTagID == DomData.tagLI || newTagId == DomData.tagUL || newTagId == DomData.tagOL;
-        ///     
-        /// (e.g. checking for any other list type opener, instead of just the same one that's currently open, as it is now) causes
-        /// the HTML5 spec document to parse differently than Chrome and these tests to fail. I am not sure why since I don't think that
-        /// OL is a valid child of UL > LI or UL is a valid child of OL > LI. But apparently they do that in the document somewhere,
-        /// e.g. open a differently-typed list as a child of an LI.
-        /// 
+        /// This method ensures that the huge DOM gets parsed correctly by checking a few key selectors. 
         /// </summary>
+        
         [ TestMethod]
         public void DomParsingTestWithNthChild()
         {
@@ -56,9 +45,11 @@ namespace HtmlParserSharp.Tests
         [ClassInitialize]
         public static void ReadLargeDoc(TestContext context)
         {
-            var parser = new CsQueryParser();
-            //Dom = CQ.Create(parser.Parse("C:\\projects\\csharp\\csquery\\source\\CsQuery.Tests\\Resources\\html standard.htm"));
-            Dom = CsQuery.Utility.Support.GetFile("HtmlParserSharp.Tests\\Resources\\html standard.htm");
+            // CsQuery (version 1.3.0 and above) uses this code.
+
+            Dom = CQ.Create(
+                CsQuery.Utility.Support.GetFile("HtmlParserSharp.Tests\\Resources\\html standard.htm")
+            );
         }
 
         
